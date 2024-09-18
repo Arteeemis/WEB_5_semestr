f019242fc6d0ef88b1739af6227a19bfa87199b6
Arteeemis <dipper3108@gmail.com>
2024-09-18 11:35:49 +0000

    lab_1

---
+++ bmstu_lab/app/views.py
@@ -0,0 +1,134 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+words = [{
+          'id' : 1, 
+          'word': 'Persistent', 
+          'level' : 'C2',
+          'language' : 'Английский',
+          'word_class' : 'adjective/прилагательное', 
+          'description': 'A persistent person continues to do something although other people do not want them to.', 
+          'translation': 'настойчивый, упорный', 
+          'example' : "he was persistent and wouldn't leave me alone", 
+          'synonyms' : 'determined, perseverant', 
+          'image' : 'http://127.0.0.1:9000/lab1/persistent.png'},
+
+         {
+          'id': 2,
+          'word': 'Alegre',
+          'level': 'A1',
+          'language': 'Испанский', 
+          'word_class': 'adjetivo/прилагательное', 
+          'description': 'Una persona alegre se siente feliz y llena de energía.',
+          'translation': 'веселый, радостный',
+          'example': 'Ella siempre está alegre y sonriente.',
+          'synonyms': 'feliz, contento',
+          'image': 'http://127.0.0.1:9000/lab1/Alegre.png'},
+
+          {
+           'id': 3,
+           'word': 'Intricate',
+           'level': 'B2',
+           'language': 'Английский',
+           'word_class': 'adjective/прилагательное', 
+           'description': 'Something intricate has many complicated details.',
+           'translation': 'сложный, запутанный',
+           'example': 'The watchmaker created an intricate design for the clock.',
+           'synonyms': 'complex, elaborate',
+           'image': 'http://127.0.0.1:9000/lab1/intricate.png'},
+
+           {
+            'id': 4,
+            'word': 'Resilient',
+            'level': 'C1',
+            'language': 'Английский', 
+            'word_class': 'adjective/прилагательное', 
+            'description': 'Someone who is resilient is able to cope well with difficulties.',
+            'translation': 'устойчивый, стойкий',
+            'example': 'She is a resilient person who has overcome many challenges.',
+            'synonyms': 'adaptable, tenacious',
+            'image': 'http://127.0.0.1:9000/lab1/resilient.png'}]
+            
+word_lists = [
+                {
+                'id': 1,
+                'items': [
+                    {
+                    'id': 1,
+                    'word': 'Persistent',
+                    'level': 'C2',
+                    'language': 'Английский',
+                    'word_class': 'adjective/прилагательное',
+                    'description': 'A persistent person continues to do something although other people do not want them to.',
+                    'translation': 'настойчивый, упорный',
+                    'example': 'he was persistent and wouldn\'t leave me alone',
+                    'synonyms': 'determined, perseverant',
+                    'image': 'http://127.0.0.1:9000/lab1/persistent.png'
+                    },
+                    {
+                    'id': 2,
+                    'word': 'Alegre',
+                    'level': 'A1',
+                    'language': 'Испанский',
+                    'word_class': 'adjetivo/прилагательное',
+                    'description': 'Una persona alegre se siente feliz y llena de energía.',
+                    'translation': 'веселый, радостный',
+                    'example': 'Ella siempre está alegre y sonriente.',
+                    'synonyms': 'feliz, contento',
+                    'image': 'http://127.0.0.1:9000/lab1/Alegre.png'
+                    }]},
+                
+                {
+                'id': 2,
+                'items': [
+                    {
+                    'id': 3,
+                    'word': 'Intricate',
+                    'level': 'B2',
+                    'language': 'Английский',
+                    'word_class': 'adjective/прилагательное',
+                    'description': 'Something intricate has many complicated details.',
+                    'translation': 'сложный, запутанный',
+                    'example': 'The watchmaker created an intricate design for the clock.',
+                    'synonyms': 'complex, elaborate',
+                    'image': 'http://127.0.0.1:9000/lab1/intricate.png'
+                    },
+                    {
+                    'id': 4,
+                    'word': 'Resilient',
+                    'level': 'C1',
+                    'language': 'Английский',
+                    'word_class': 'adjective/прилагательное',
+                    'description': 'Someone who is resilient is able to cope well with difficulties.',
+                    'translation': 'устойчивый, стойкий',
+                    'example': 'She is a resilient person who has overcome many challenges.',
+                    'synonyms': 'adaptable, tenacious',
+                    'image': 'http://127.0.0.1:9000/lab1/resilient.png'
+                    }]}
+             ]
+
+def words_page(request):
+    return render(request, 'index.html')
+
+def GetWordCards(request):
+    return render(request, 'base_card.html', {'data' : {
+        'word_cards' : words
+    }})
+
+def GetWordCard(request, id):
+    for word in words:
+        if word['id'] == id:
+            return render(request, 'about.html', {'data' : word})
+        
+def GetWordLists(request,id):
+    for word_list in word_lists:
+        if word_list['id'] == id:
+            return render(request, 'cart_card.html', {'data' : word_list['items']})
+
+def search(request):
+    if request.method == 'POST':
+        query = request.POST.get('query')
+        results = []
+        if query:
+            results = [word for word in words if query.lower() in word['word'].lower()]
+        return render(request, 'base_card.html', {'data': {'word_cards' : results}})
---
+++ bmstu_lab/bmstu_lab/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for bmstu_lab project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('words_page/', views.words_page),
+    path('', views.GetWordCards),
+    path('card/<int:id>/', views.GetWordCard, name='card_url'),
+    path('list/<int:id>/', views.GetWordLists, name='lists_url'),
+    path('search/', views.search, name='search')
+]
+
+
